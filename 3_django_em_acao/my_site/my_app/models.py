f9d65a82aed954b4540a220b48ee7a63f9a3fec0
Eliakin Costa <eliakim170@gmail.com>
2019-03-24 00:14:12 -0300

    Django em acao Models e Admin

--- 3_django_em_acao/my_site/my_app/models.py
+++ 3_django_em_acao/my_site/my_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=250)
---
+++ 3_django_em_acao/my_site/my_app/tests/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from .models import Pessoa
+
+
+#class PessoaModelTest(TestCase):
+
+    #def test_string_representation(self):
+        #pessoa = Pessoa(nome="Eliakin", sobrenome="Costa")
+        #self.assertEqual(str(pessoa), "{0} {1}".format(pessoa.nome, pessoa.sobrenome)) 
