2afd30130cceca75cc975c47fce72b7fd56b587e
Eliakin Costa <eliakim170@gmail.com>
2019-03-23 23:04:14 -0300

    Adicionado Language e Timezone do Brasil

--- 3_django_em_acao/my_site/my_site/settings.py
+++ 3_django_em_acao/my_site/my_site/settings.py
@@ -105,8 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'pt_BR'
 
 TIME_ZONE = 'UTC'
+#TIME_ZONE = 'America/Bahia'
 
 USE_I18N = True
 
